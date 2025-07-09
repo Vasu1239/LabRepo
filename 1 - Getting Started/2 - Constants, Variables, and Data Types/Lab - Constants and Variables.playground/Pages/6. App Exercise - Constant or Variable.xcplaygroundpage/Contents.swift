2b7083eadfbea8548e071374a92ff38d92cc15bf
Vasu1239 <vasu1239.be23@chitkara.edu.in>
2025-07-09 11:37:38 +0530

    Unit-1 & Unit-2 labs are added

---
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/6. App Exercise - Constant or Variable.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+/*:
+## App Exercise - Fitness Tracker: Constant or Variable?
+ 
+ >These exercises reinforce Swift concepts in the context of a fitness tracking app.
+ 
+ There are all sorts of things that a fitness tracking app needs to keep track of in order to display the right information to the user. Similar to the last exercise, declare either a constant or a variable for each of the following items, and assign each a sensible value. Be sure to use proper naming conventions.
+ 
+- Name: The user's name
+- Age: The user's age
+- Number of steps taken today: The number of steps that a user has taken today
+- Goal number of steps: The user's goal for number of steps to take each day
+- Average heart rate: The user's average heart rate over the last 24 hours
+ */
+let userName: String = "Vasu"
+print("User's name is always a constant")
+var userAge:Int = 19
+print("User's age will change yearly")
+var numberOfStepsTakenToday:Int = 1000
+print("User's steps taken is not constant value")
+let goalNumberOfSteps:Int = 15000
+print("User's step goal is constant")
+var averageHeartRate:Int = 70
+print("User's heart rate is not constant")
+
+
+/*:
+ Now go back and add a line after each constant or variable declaration. On those lines, print a statement explaining why you chose to declare the piece of information as a constant or variable.
+ 
+[Previous](@previous)  |  page 6 of 10  |  [Next: Exercise - Types and Type Safety](@next)
+ */
---
+++ 2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,45 @@
+/*:
+## Exercise - Create Functions
+
+ Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the function and observe the printout.
+ */
+func introduceMySelf(){
+    print("Hello,I am Vasu ,persuating B.tech in CSE")
+}
+introduceMySelf()
+
+//:  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = Int.random(in: 0...4)` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts.
+import Foundation
+
+func magicEightBall() {
+    let randomNum = Int.random(in: 0...4)
+    switch randomNum {
+    case 0:
+        print("The number is \(randomNum)")
+        
+    case 1:
+        print("The number is \(randomNum)")
+        
+    case 2:
+        print("The number is \(randomNum)")
+        
+    case 3:
+        print("The number is \(randomNum)")
+        
+    case 4:
+        print("The number is \(randomNum)")
+        
+    default:
+        print("The number is \(randomNum)")
+    }
+}
+magicEightBall()
+magicEightBall()
+magicEightBall()
+magicEightBall()
+magicEightBall()
+magicEightBall()
+magicEightBall()
+/*:
+page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
+ */

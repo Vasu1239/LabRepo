de020282208d33cd61f640f16281376e1832feab
Vasu1239 <vasu1239.be23@chitkara.edu.in>
2025-07-21 20:27:53 +0530

    Assignment added

---
+++ TabBar/TabBar/RedViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabBar
+//
+//  Created by Vasu_SKH on 21/07/25.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        tabBarItem.badgeValue = "10"
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarItem.badgeValue = nil
+    }
+
+
+}
+
---
+++ TabBar/TabBar/YellowViewController.swift
@@ -0,0 +1,30 @@
+//
+//  YellowViewController.swift
+//  TabBar
+//
+//  Created by Vasu_SKH on 21/07/25.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarItem.badgeValue = "1"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
